4f14be01640ff8ff066e6690a487f654d77ec10b
Drew Peterson <drew.a.peterson@hotmail.com>
2021-07-18 14:35:15 -0700

    Form functionaly writing to the state for produce

--- src/components/ProductController.js
+++ src/components/ProductController.js
@@ -14,275 +14,32 @@ class ProductController extends React.Component {
       selectedProduct: null,
       editing: false,
       availableProduce: [  
-        {  
-           month: "January",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Garlic",
-              "Mushrooms",
-              "Onions",
-              "Potatoes",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "February",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Garlic",
-              "Mushrooms",
-              "Onions",
-              "Potatoes"
-           ]
-        },
-        {  
-           month: "March",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Rhubarb",
-              "Garlic",
-              "Mushrooms",
-              "Onions",
-              "Potatoes"
-           ]
-        },
-        {  
-           month: "April",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Rhubarb",
-              "Asparagus",
-              "Garlic",
-              "Lettuce",
-              "Mushrooms",
-              "Onions",
-              "Potatoes"
-           ]
-        },
-        {  
-           month: "May",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Rhubarb",
-              "Asparagus",
-              "Cauliflower",
-              "Garlic",
-              "Lettuce",
-              "Potatoes",
-              "Radishes"
-           ]
-        },
-        {  
-           month: "June",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Rhubarb",
-              "Blackberries",
-              "Cherries",
-              "Raspberries",
-              "Strawberries",
-              "Asparagus",
-              "Broccoli",
-              "Cauliflower",
-              "Kohlrabi",
-              "Lettuce",
-              "Mushrooms",
-              "Potatoes",
-              "Radishes",
-              "Squash"
-           ]
-        },
-        {  
-           month: "July",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Rhubarb",
-              "Apricots",
-              "Blackberries",
-              "Blueberries",
-              "Cherries",
-              "Melons",
-              "Nectarines",
-              "Peaches",
-              "Raspberries",
-              "Strawberries",
-              "Tomatoes",
-              "Beets",
-              "Broccoli",
-              "Brussel Sprouts",
-              "Cabbage",
-              "Carrots",
-              "Cauliflower",
-              "Cucumber",
-              "Eggplant",
-              "Garlic",
-              "Green Beans",
-              "Kohlrabi",
-              "Lettuce",
-              "Mushrooms",
-              "Potatoes",
-              "Radishes",
-              "Squash",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "August",
-           selection: [  
-              "Apples",
-              "Apricots",
-              "Blackberries",
-              "Blueberries",
-              "Cherries",
-              "Melons",
-              "Nectarines",
-              "Peaches",
-              "Pears",
-              "Plums",
-              "Raspberries",
-              "Rhubarb",
-              "Strawberries",
-              "Tomatoes",
-              "Beets",
-              "Broccoli",
-              "Brussel Sprouts",
-              "Cabbage",
-              "Carrots",
-              "Cauliflower",
-              "Corn",
-              "Cucumber",
-              "Eggplant",
-              "Garlic",
-              "Green Beans",
-              "Kohlrabi",
-              "Lettuce",
-              "Mushrooms",
-              "Onions",
-              "Peas",
-              "Peppers",
-              "Potatoes",
-              "Radishes",
-              "Squash",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "September",
-           selection: [  
-              "Apples",
-              "Blueberries",
-              "Grapes",
-              "Melons",
-              "Peaches",
-              "Pears",
-              "Plums",
-              "Raspberries",
-              "Tomatoes",
-              "Broccoli",
-              "Brussel Sprouts",
-              "Cabbage",
-              "Carrots",
-              "Cauliflower",
-              "Corn",
-              "Cucumber",
-              "Eggplant",
-              "Garlic",
-              "Green Beans",
-              "Kohlrabi",
-              "Lettuce",
-              "Mushrooms",
-              "Onions",
-              "Peas",
-              "Peppers",
-              "Potatoes",
-              "Radishes",
-              "Squash",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "October",
-           selection: [  
-              "Apples",
-              "Grapes",
-              "Hazelnuts",
-              "Melons",
-              "Peaches",
-              "Pears",
-              "Tomatoes",
-              "Broccoli",
-              "Brussel Sprouts",
-              "Cabbage",
-              "Carrots",
-              "Cauliflower",
-              "Corn",
-              "Cucumber",
-              "Eggplant",
-              "Garlic",
-              "Green Beans",
-              "Kohlrabi",
-              "Lettuce",
-              "Mushrooms",
-              "Onions",
-              "Peas",
-              "Peppers",
-              "Potatoes",
-              "Pumpkins",
-              "Radishes",
-              "Squash",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "November",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-              "Broccoli",
-              "Carrots",
-              "Cauliflower",
-              "Garlic",
-              "Mushrooms",
-              "Onions",
-              "Potatoes",
-              "Squash",
-              "Turnips"
-           ]
-        },
-        {  
-           month: "December",
-           selection: [  
-              "Apples",
-              "Hazelnuts",
-              "Pears",
-             "Broccoli",
-              "Carrots",
-              "Cauliflower",
-              "Garlic",
-              "Mushrooms",
-              "Onions",
-              "Potatoes",
-              "Turnips"
-           ]
-        }
+      //   {  
+      //      month: "January",
+      //      id: "a21101",
+      //      key: "a21101",
+      //      selection: [  
+      //         "Apples",
+      //         "Hazelnuts",
+      //         "Pears",
+      //         "Garlic",
+      //         "Mushrooms",
+      //         "Onions",
+      //         "Potatoes",
+      //         "Turnips"
+      //      ]
+      //   }
      ]
 
     }
+   }
+
+   handleAddNewProductsList = (newProduce) => {
+      const newProduceList = this.state.availableProduce.concat(newProduce);
+      this.setState({
+         formVisibleOnPage: false,
+         availableProduce: newProduceList
+      });
    }
 
    handleClick = () => {
@@ -311,7 +68,7 @@ class ProductController extends React.Component {
       currentlyVisibleState = <ProductDetail />
       buttonText = "Return to Product List";
    } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewProduct />
+      currentlyVisibleState = <NewProduct onProduceCreation= { this.handleAddNewProductsList }/>
       buttonText = "Return to Product List";
    } else {
       currentlyVisibleState = <ProductList availableProduce={this.state.availableProduce} />
